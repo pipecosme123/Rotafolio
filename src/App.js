e57731676f47c9a616c47946b5bce6c1ec0d6acf
Daniel Cosme <pipecosme123@gmail.com>
2021-08-23 16:17:00 -0500

    Se añade el seguimiento de Google Analitycs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import ReactGA from 'react-ga';
 import Folleto from './Components/Folleto';
 import Rotafolio from './Components/Rotafolio';
+import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('UA-205618770-1');
+    // To Report Page View 
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/duraphat" component={Rotafolio} />
-          <Route exact path="/folleto" component={Folleto} />
+          <Route exact path="/duraphat" component={() => <Rotafolio></Rotafolio>} />
+          <Route exact path="/folleto" component={() => <Folleto></Folleto>} />
         </Switch>
       </Router>
     </div>
--- src/Components/Rotafolio.js
+++ src/Components/Rotafolio.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SlidersRotafolio } from '../Constants/Imagenes';
+import ReactGA from 'react-ga';
 import "../css/Rotafolio.css";
 
 import { toast } from 'react-toastify';
@@ -14,44 +15,69 @@ import 'swiper/components/scrollbar/scrollbar.scss';
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 toast.configure()
+
 const Rotafolio = () => {
+
   let orientacion = window.orientation;
   let resolucion = window.screen.width;
   window.document.title = 'Duraphat - Rotafolio';
 
   const [show, setShow] = useState(false);
+  // let numSlider = 0;
+  const [numSlider, setNumSlider] = useState(0);
 
-  function notify(){
-    toast.info('Te recomendamos rotar la pantalla de tu dispositivo móvil para tener una mayor experiencia',{position: toast.POSITION.TOP_CENTER, autoClose: 8000});
+
+  const ckickGoogleAnalitycs = (nums) => {
+    // console.log(nums);
+    if (nums === 22) {
+      ReactGA.event({
+        category: 'Click',
+        action: `Ingresaron al folleto de Duraphat`,
+      });
+    }
   }
-  
-  function info(){
-    toast.info('Puedes dar click a esta imagen',{position: toast.POSITION.TOP_CENTER, autoClose: 5000});
+
+  const notify = (num) => {
+    if (num === 1) {
+      toast.info('Te recomendamos rotar la pantalla de tu dispositivo móvil para tener una mejor experiencia', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 8000,
+        toastId: "noti1"
+      });
+    } else if (num === 2) {
+      toast.info('Puedes dar click a esta imagen', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+        toastId: "noti2"
+      });
+    }
   }
 
   useEffect(() => {
-    if(resolucion < 500 && orientacion === 0){
-      notify();
+    if (resolucion < 500 && orientacion === 0 && numSlider === 0) {
+      notify(1);
     }
-  
-    if(show){
-      info();
+
+    if (show) {
+      notify(2);
     }
   })
 
   return (
     <div className="carrousel">
+
       <Swiper
         spaceBetween={50}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
+        onSlideChange={(swiper) => { setNumSlider(swiper.activeIndex); swiper.activeIndex === 22 ? setShow(true) : setShow(false);}}
       >
         {SlidersRotafolio.map(slider => (
           <SwiperSlide key={slider._id}>
             {({ isActive }) => (
-              <div onChange={ isActive && slider._id === 22 ?  setShow(true) : setShow(false)}>
-                <a href={slider.href}>
+              <div>
+                <a href={slider.href} target="blank" onClick={() => ckickGoogleAnalitycs(numSlider)}>
                   <img className="img_slider" src={slider.Slider} alt="" />
                 </a>
               </div>
@@ -60,7 +86,10 @@ const Rotafolio = () => {
 
         ))}
       </Swiper>
-      {/* <p className={show ? "activate" : "noActivate"} id="notif">Puedes dar click <br></br> a esta imagen</p> */}
+
+      {/* <p className={show ? "activate" : "noActivate"} id="notif">Puedes dar click <br></br> a esta imagen</p>
+      onChange={ isActive && slider._id === 22 ?  setShow(true) : setShow(false)}   {slider.href}
+      */}
     </div>
   );
 
